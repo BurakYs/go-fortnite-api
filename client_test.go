d1ea7cd6471f05e0656cee01febb8dd275fddeb6
BurakYs <brkyhs0@gmail.com>
2025-08-05 01:00:01 +0300

    test: add t.Parallel() and use require

--- client_test.go
+++ client_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -28,6 +29,7 @@ var (
 )
 
 func requireAPIKey(t *testing.T) {
+	t.Helper()
 	if err := testClient.checkAPIKey(); err != nil {
 		t.Skip("API_KEY is not set")
 	}
@@ -47,92 +49,109 @@ func TestMain(m *testing.M) {
 }
 
 func Test_GetAESKey(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetAESKey(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBanners(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBanners(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBannerColors(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBannerColors(testCtx)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetAllCosmetics(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetAllCosmetics(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetNewCosmetics(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetNewCosmetics(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBRCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBRCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetTrackCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetTrackCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetInstrumentCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetInstrumentCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetCarCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetCarCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetLegoCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetLegoCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetLegoKitCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetLegoKitCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBeanCosmeticsList(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBeanCosmeticsList(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBRCosmeticByID(t *testing.T) {
+	t.Parallel()
 	resp, err := testClient.GetBRCosmeticByID(testCtx, testCosmeticID1, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, testCosmeticID1, resp.ID)
 }
 
 func Test_GetBRCosmeticByID_WithEmptyID(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBRCosmeticByID(testCtx, "", nil)
-	assert.ErrorIs(t, err, ErrEmptyParameter)
+	require.ErrorIs(t, err, ErrEmptyParameter)
 }
 
 func Test_SearchBRCosmetic(t *testing.T) {
+	t.Parallel()
 	resp, err := testClient.SearchBRCosmetic(testCtx, &SearchBRCosmeticParams{Name: testCosmeticName})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, testCosmeticName, resp.Name)
 }
 
 func Test_SearchBRCosmetics(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.SearchBRCosmetics(testCtx, &SearchBRCosmeticsParams{Name: testCosmeticName})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_SearchBRCosmeticsByIDs(t *testing.T) {
+	t.Parallel()
 	ids := []string{testCosmeticID1, testCosmeticID2}
 	resp, err := testClient.SearchBRCosmeticsByIDs(testCtx, ids, nil)
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Len(t, resp, 2)
 
 	for _, cosmetic := range resp {
@@ -141,77 +160,91 @@ func Test_SearchBRCosmeticsByIDs(t *testing.T) {
 }
 
 func Test_SearchBRCosmeticsByIDs_WithEmptyIDs(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.SearchBRCosmeticsByIDs(testCtx, []string{}, nil)
 	assert.ErrorIs(t, err, ErrEmptyParameter)
 }
 
 func Test_GetCreatorCode(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetCreatorCode(testCtx, testCreatorCode, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetCreatorCode_WithEmptyCode(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetCreatorCode(testCtx, "", nil)
 	assert.ErrorIs(t, err, ErrEmptyParameter)
 }
 
 func Test_GetBRMap(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBRMap(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetNews(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetNews(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBRNews(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetBRNews(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetSTWNews(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetSTWNews(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetCreativeNews(t *testing.T) {
+	t.Parallel()
 	t.Skip("Creative news are not available anymore")
 
 	_, err := testClient.GetCreativeNews(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetPlaylists(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetPlaylists(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetPlaylistByID(t *testing.T) {
+	t.Parallel()
 	resp, err := testClient.GetPlaylistByID(testCtx, testPlaylistID, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, testPlaylistID, resp.ID)
 }
 
 func Test_GetPlaylistByID_WithEmptyID(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetPlaylistByID(testCtx, "", nil)
 	assert.ErrorIs(t, err, ErrEmptyParameter)
 }
 
 func Test_GetShop(t *testing.T) {
+	t.Parallel()
 	_, err := testClient.GetShop(testCtx, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func Test_GetBRStatsByName(t *testing.T) {
+	t.Parallel()
 	requireAPIKey(t)
 
 	resp, err := testClient.GetBRStatsByName(testCtx, testStatsName, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, testStatsName, resp.Account.Name)
 }
 
 func Test_GetBRStatsByName_WithEmptyName(t *testing.T) {
+	t.Parallel()
 	requireAPIKey(t)
 
 	_, err := testClient.GetBRStatsByName(testCtx, "", nil)
@@ -219,14 +252,16 @@ func Test_GetBRStatsByName_WithEmptyName(t *testing.T) {
 }
 
 func Test_GetBRStatsByAccountID(t *testing.T) {
+	t.Parallel()
 	requireAPIKey(t)
 
 	resp, err := testClient.GetBRStatsByID(testCtx, testStatsID, nil)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, testStatsID, resp.Account.ID)
 }
 
 func Test_GetBRStatsByAccountID_WithEmptyID(t *testing.T) {
+	t.Parallel()
 	requireAPIKey(t)
 
 	_, err := testClient.GetBRStatsByID(testCtx, "", nil)
